1123db32d19b5da3bb30adbb1c2d70cf087a4c85
kangaroo-helloworldlabs <kangaroo@helloworldlabs.kr>
2026-01-06 11:42:34 +0900

    'update'

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -7,7 +7,7 @@ export default function UserMainPage() {
     <div className="mx-auto flex max-w-5xl flex-col gap-6 py-8">
       <header className="flex items-center justify-between">
         <div>
-          <h1 className="text-3xl font-bold">사용자 측 메인 화면</h1>
+          <h1 className="text-3xl font-bold">사용자 측 메인 화면 - 수정수정!!!!</h1>
         </div>
       </header>
 
